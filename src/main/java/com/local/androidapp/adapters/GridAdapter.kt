7dbd3b8243d03c9440f77e33acb549b803d36d43
vicarb <vcardenasb@protonmail.com>
2023-09-13 23:13:10 -0300

    updated adapter

--- src/main/java/com/local/androidapp/adapters/GridAdapter.kt
+++ src/main/java/com/local/androidapp/adapters/GridAdapter.kt
@@ -1,6 +1,6 @@
-
 package com.local.androidapp.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +10,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.local.androidapp.R
 import com.local.androidapp.models.GridItem
 
-class GridAdapter(private val items: List<GridItem>) : RecyclerView.Adapter<GridAdapter.GridViewHolder>() {
+class GridAdapter(
+    private val items: List<GridItem>,
+    private val onClick: ((GridItem) -> Unit)? = null // <-- Click event
+) : RecyclerView.Adapter<GridAdapter.GridViewHolder>() {
 
-    class GridViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class GridViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val name: TextView = view.findViewById(R.id.grid_item_name)
         val image: ImageView = view.findViewById(R.id.grid_item_image)
+
+        // Initialize item click listener
+        init {
+            view.setOnClickListener {
+                val position = bindingAdapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    onClick?.invoke(items[position])
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridViewHolder {
@@ -23,6 +36,7 @@ class GridAdapter(private val items: List<GridItem>) : RecyclerView.Adapter<Grid
     }
 
     override fun onBindViewHolder(holder: GridViewHolder, position: Int) {
+        Log.d("GridAdapter", "Binding view holder at position $position")
         val item = items[position]
         holder.name.text = item.name
         holder.image.setImageResource(item.imageResourceId)
